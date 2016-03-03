4e67542e4ef1438e219524217979198ebb5b0c76
Ami <agianchandani2018@pingry.org>
2016-03-03 10:03:39 -0500

    2nd version full function

--- tenCharacters.py
+++ tenCharacters.py
@@ -2,11 +2,20 @@
 #Date: March 4
 
 
-f = open("input.txt", "r+")
-text = f.read()
+f = open("input.txt", "r")
+lines = f.readlines()
 f.close()
 
-#print(text)
-
 f = open("output.txt", "w")
-f.write(text + "\n")
+
+for word in lines:
+	if len(word) < 11:
+		
+		f.write(word)
+		
+	if len(word) > 10:
+		number = len(word)
+		left = number - 10
+		word = word[: - left]
+		f.write(word + "\n")
+	
